0fbd85ad80d86c1669baeecaf0165a59d2f7ff68
Ryan Lee <hi@iamryanlee.com>
2020-04-30 21:49:11 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import AddToCalendar from "./components/AddToCalendar";
+
+const event = {
+  duration: "5",
+  endDatetime: "2020-01-01",
+  startDatetime: "2020-01-01",
+  title: "Event title",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddToCalendar event={event} />
     </div>
   );
 }
---
+++ src/components/AddToCalendar/AddToCalendar.tsx
@@ -0,0 +1,71 @@
+import React, { useRef, useEffect, RefObject } from "react";
+import AddToCalendarHOC from "react-add-to-calendar-hoc";
+
+interface IButtonProps {
+  onClick: () => void;
+}
+
+const Button: React.FC<IButtonProps> = ({ children, onClick }) => {
+  return (
+    <div className="AddToCalendar-button" onClick={onClick}>
+      {children}
+    </div>
+  );
+};
+
+const Dropdown: React.FC = ({ children }) => {
+  return <div className="AddToCalendar-dropdown">{children}</div>;
+};
+
+interface IAddToCalendarProps {
+  event: {
+    description?: string;
+    duration: string | number;
+    endDatetime: string;
+    location?: string;
+    startDatetime: string;
+    timezone?: string;
+    title: string;
+  };
+  buttonText?: string;
+}
+
+export const AddToCalendar: React.FC<IAddToCalendarProps> = ({
+  buttonText = "Add to Calendar",
+  event,
+}) => {
+  const AddToCalendar = AddToCalendarHOC(Button, Dropdown);
+  const hocRef = useRef() as RefObject<any>; // set your RefObject type accordingly
+
+  const handleClick = (e: MouseEvent) => {
+    const element = e.target as HTMLElement;
+
+    if (
+      element &&
+      element.className !== "AddToCalendar-button" &&
+      hocRef.current.state.dropdownOpen
+    ) {
+      hocRef.current.handleDropdownToggle(e);
+    }
+  };
+
+  useEffect(() => {
+    console.log(hocRef);
+
+    if (hocRef) {
+      document.addEventListener("click", handleClick);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return (
+    <div className="AddToCalendar" title="Add to your calendar">
+      <AddToCalendar buttonText={buttonText} event={event} ref={hocRef} />
+    </div>
+  );
+};
+
+export default AddToCalendar;
